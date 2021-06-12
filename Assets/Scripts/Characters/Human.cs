918a8596927b9f3c9726d2e5d45db320e8a30324
Sasha <reallsasha@gmail.com>
2021-06-12 16:23:21 +0200

    Lots of assets and navmesh and tilemap

--- Assets/Scripts/Characters/Human.cs
+++ Assets/Scripts/Characters/Human.cs
@@ -2,17 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Human : Ai
 {
+    private NavMeshAgent agent;
     private void Start()
     {
         CharacterManager.registerHuman(this);
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
     }
 
     private void FixedUpdate()
     {
+        print($"dest:{agent.destination}|player:{Player.instance.transform.position}");
         // print("visible:" + checkVisisble(Player.instance.gameObject));
+        agent.SetDestination(Player.instance.transform.position);
 
         if (Input.GetKeyUp(KeyCode.Z))
         {
