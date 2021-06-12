caa2234347502a191c7d6addb82f355d3214418a
shaneacton <reallysasha@gmail.com>
2021-06-12 10:57:52 +0200

    started zombification

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,33 @@ public abstract class Character : MonoBehaviour
     public float moveSpeed;
     public float visionDistance;
     public float visionAngle;
-    public float infectionTime;
+    public float infectionTime; // how much time from infection until zombification
+    private float timeOfInfection;  // -1 if not infected
+
 
     public abstract void die();
+    
+    public void infect()
+    {
+        timeOfInfection = Time.time;
+        StartCoroutine(WaitAndZombify());
+    }
+
+    private IEnumerator WaitAndZombify()
+    {
+        yield return new WaitForSeconds(infectionTime);
+        zombify();
+    }
+
+    private void zombify()
+    {
+        Player player = GetComponent<Player>();
+        if (player != null)
+        {
+            throw new Exception("player is in zombified character");
+        }
+
+        Human human = GetComponent<Human>();
+        CharacterManager.zombify(human);
+    }
 }
--- Assets/Scripts/Characters/Human.cs
+++ Assets/Scripts/Characters/Human.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 
 public class Human : Ai
 {
-    
+    private void Start()
+    {
+        CharacterManager.registerHuman(this);
+    }
+
 
     private void FixedUpdate()
     {
         print("visible:" + checkVisisble(Player.instance.gameObject));
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            character.infect();
+        }
     }
 }
