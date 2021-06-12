2940224d8bc9c0f0b4550df32091e132048fb5ef
Liron Toledo <ldoggy20@gmail.com>
2021-06-12 14:02:05 +0200

    Rotate player with mouse

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -25,6 +25,14 @@ public class Player : Controller
         move();
         findHosts();
         tryAttack();
+        handleRotation();
+    }
+
+    void handleRotation()
+    {
+        Vector3 mouseScreen = Input.mousePosition;
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(mouseScreen);
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg - 90);
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
