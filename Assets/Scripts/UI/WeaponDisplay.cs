82683edd2af10359409dbc7dfa12e1afa87e5a16
shaneacton <reallysasha@gmail.com>
2021-06-13 14:55:17 +0200

    weapon display ui

---
+++ Assets/Scripts/UI/WeaponDisplay.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponDisplay : MonoBehaviour
+{
+    public Image display;
+    public Sprite gun;
+    public Sprite fist;
+    private void Awake()
+    {
+        display = GetComponent<Image>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.instance.character is Sauce)
+        {
+            display.color = new Color(0,0,0,0);
+        }
+
+        if (Player.instance.character is Ranged)
+        {
+            display.sprite = gun;
+            display.color = Color.white;
+        }
+        if (Player.instance.character is Melee)
+        {
+            display.sprite = fist;
+            display.color = Color.white;
+        }
+    }
+}
