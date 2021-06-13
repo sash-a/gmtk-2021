6c4d4b27351e16c2e00815d298a52a9d3446b0db
Sasha <reallsasha@gmail.com>
2021-06-13 12:44:05 +0200

    Added ranged attacks

--- Assets/Scripts/Characters/Attacker.cs
+++ Assets/Scripts/Characters/Attacker.cs
@@ -7,4 +7,20 @@ public abstract class Attacker : Character
     public float attackRange; // distance guard will stop and shoot at
     public abstract void Attack(Vector3 dir = new Vector3(), bool isPlayer = false);
 
+    public bool checkCleanLineSight()
+    {    
+        int layerMask = LayerMask.GetMask("player", "zombie", "wall");
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, attackRange, layerMask);
+        if (hitInfo)
+        {
+            Character character = hitInfo.transform.GetComponent<Character>();
+
+            if (character)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
