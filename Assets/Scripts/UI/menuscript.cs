6deb750b4c6dde0e227c10bfe6609fe05bfda241
Andy <andrewdenton96@gmail.com>
2021-06-13 16:19:55 +0200

    fix guard anim and menu

--- Assets/Scripts/UI/menuscript.cs
+++ Assets/Scripts/UI/menuscript.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class menuscript : MonoBehaviour
+public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void QuitGame() {
+        Application.Quit();
     }
 }
