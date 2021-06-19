6e92a735d014e540927dcf09c8fcd6508e265fb3
shaneacton <shaneacton745.sa@gmail.com>
2021-06-19 15:38:06 +0200

    levels

--- Assets/Scripts/Characters/Ai/Waypoints.cs
+++ Assets/Scripts/Characters/Ai/Waypoints.cs
@@ -6,15 +6,26 @@ using UnityEngine;
 public class Waypoints : MonoBehaviour
 {
     [NonSerialized] private List<Vector3> waypoints;
+    public bool cyclical;
     
     public Vector3 this[int index]
     {
-        get => waypoints[index];
+        get => waypoints[index % waypoints.Count];
     }
 
     public int Count
     {
-        get => waypoints.Count;
+        get
+        {
+            if (cyclical)
+            {
+                return int.MaxValue;
+            }
+            else
+            {
+                return waypoints.Count;
+            }
+        }
     }
 
     public void compute()
