28f2b63ba05df335b98fa0663f167d10d2696b9e
sash-a <reallysasha@gmail.com>
2021-07-07 22:58:20 +0200

    Added waypoint patrol on search

--- Assets/Scripts/Characters/Ai/WaypointsGenerator.cs
+++ Assets/Scripts/Characters/Ai/WaypointsGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 public class WaypointsGenerator : MonoBehaviour
@@ -10,14 +11,14 @@ public class WaypointsGenerator : MonoBehaviour
     private Controller controller;
     int layerMask = -1;
 
-    public List<Vector3> getCircleAroundPoint(int numPoints, float radius, float randomNess=0)
+    /*
+     * randomness: [0,1], affects angle and radius randomness
+     */
+    public List<Vector3> getCircleAroundPoint(int numPoints, float radius, float randomness = 0)
     {
-        /*
-         * randomness: [0,1], affects angle and radius randomness
-         */
         if (layerMask == -1)
         {
-            layerMask = LayerMask.GetMask("obstacles", "wall");
+            layerMask = LayerMask.GetMask("wall");
         }
 
         float angleInc = 2 * Mathf.PI / numPoints;
@@ -27,21 +28,21 @@ public class WaypointsGenerator : MonoBehaviour
         {
             Vector3 candPoint = transform.position + Vector3.zero;
             float angle = angleInc * i;
-            float angleRandom = Random.Range(-angleInc, angleInc) * randomNess;
-            float radiusRandom = Random.Range(-radius, radius/2) * randomNess;
+            float angleRandom = Random.Range(-angleInc, angleInc) * randomness;
+            float radiusRandom = Random.Range(-radius, radius / 2) * randomness;
 
             candPoint[0] += Mathf.Sin(angle + angleRandom) * (radius + radiusRandom);
             candPoint[1] += Mathf.Cos(angle + angleRandom) * (radius + radiusRandom);
 
-            if (isWaypointVisisble(candPoint)) 
+            if (isWaypointVisisbleAndReachable(candPoint))
             {
                 circle.Add(candPoint);
             }
             else
             {
-                if (randomNess > 0 && remainingTries > 0) // non deterministic, try again
+                if (randomness > 0 && remainingTries > 0) // non deterministic, try again
                 {
-                    i --; // send the loop back to the current point to try again
+                    i--; // send the loop back to the current point to try again
                     remainingTries--;
                 }
                 else
@@ -60,15 +61,25 @@ public class WaypointsGenerator : MonoBehaviour
         return circle;
     }
 
-    private bool isWaypointVisisble(Vector3 waypoint)
-    { // raycasts from current pos to waypoint, checks if any obstructions
-        Vector2 diff = transform.position - waypoint;
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, diff, diff.magnitude, layerMask);
+    private bool isWaypointVisisbleAndReachable(Vector3 waypoint)
+    {
+        // raycasts from current pos to waypoint, checks if any obstructions
+        var position = transform.position;
+
+        Vector2 diff = position - waypoint;
+        RaycastHit2D hitInfo = Physics2D.Raycast(position, diff, diff.magnitude, layerMask);
         if (hitInfo)
         {
             return false;
         }
 
-        return true;
+        // Is visible, check if reachable
+        var reachable = true;
+        if (controller is Ai ai)
+        {
+            reachable = ai.agent.CalculatePath(waypoint, new NavMeshPath());
+        }
+
+        return reachable;
     }
 }
