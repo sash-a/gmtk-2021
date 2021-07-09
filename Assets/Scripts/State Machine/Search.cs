7e09d36c81540359b74a5cc5594c9b53edbf9113
sash-a <reallysasha@gmail.com>
2021-07-09 11:53:31 +0200

    Changed search behaviour again and update UI colours

--- Assets/Scripts/State Machine/Search.cs
+++ Assets/Scripts/State Machine/Search.cs
@@ -11,19 +11,14 @@ namespace State_Machine
         private int _maxSearchTime;
         private float _searchTimePassed;
 
-        private List<Vector3> searchPoints;
-        private int _wayptIdx;
         private float _distThresh = 1f;
-        private bool _initRoute;
 
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             _controller = animator.GetComponent<Ai>();
 
-            _maxSearchTime = Random.Range(2, 5);
-            _wayptIdx = 0;
+            _maxSearchTime = Random.Range(3, 5);
             _searchTimePassed = 0f;
-            _initRoute = true;
 
             if (_controller is Human && _controller.character.getInfectionFrac() == -1)
             {
@@ -35,8 +30,8 @@ namespace State_Machine
                 _controller.visibilityIcon.setText("");
             }
 
-            searchPoints =
-                _controller.character.waypoints.wayGen.getCircleAroundPoint(Random.Range(2, 4), 3, 0.5f);
+            _controller.ClearAgentPath();
+            _controller.agent.SetDestination(Player.instance.transform.position);
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -46,12 +41,6 @@ namespace State_Machine
                 _controller = animator.GetComponent<Ai>();
             }
 
-            foreach (var point in searchPoints)
-            {
-                Debug.DrawLine(_controller.transform.position, point, Color.blue);
-            }
-
-            //Debug.Log("searching");
             _searchTimePassed += Time.deltaTime;
             if (_searchTimePassed > _maxSearchTime)
             {
@@ -66,9 +55,10 @@ namespace State_Machine
             }
             //Debug.Log("no targets");
 
-            // animator.transform.RotateAround(animator.transform.position, Vector3.forward, 100 * Time.deltaTime);
-            WaypointPatrol(_controller.transform, _initRoute);
-            _initRoute = false;
+            if (Vector2.Distance(animator.transform.position, _controller.agent.destination) < _distThresh)
+            {
+                animator.transform.RotateAround(animator.transform.position, Vector3.forward, 50 * Time.deltaTime);
+            }
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -76,36 +66,10 @@ namespace State_Machine
             _controller.visibilityIcon.setText("");
         }
 
-        // TODO this mostly a duplicate of Patrol.WaypointPatrol really should make it general
-        void WaypointPatrol(Transform transform, bool initialRoute = false)
-        {
-            // if (_controller == null)
-            // {
-            //     _controller = _character.GetComponent<Ai>();
-            // }
-
-            if (Vector2.Distance(transform.position, searchPoints[_wayptIdx]) < _distThresh || initialRoute)
-            {
-                _wayptIdx++;
-                _wayptIdx %= searchPoints.Count;
-                _controller.agent.SetDestination(searchPoints[_wayptIdx]);
-                // Debug.Log($"Waypoint set at :{_character.waypoints[_wayptIdx]}");
-            }
-
-            var position = _controller.transform.position;
-            Debug.DrawLine(position, searchPoints[_wayptIdx], Color.green);
-            try
-            {
-                Debug.DrawLine(position, _controller.agent.destination, Color.blue);
-            }
-            catch (Exception)
-            {
-                /* ignored */
-            }
-        }
-
         private void Chase(Animator anim)
         {
+            _controller.ClearAgentPath();
+
             anim.SetBool(AnimatorFields.Chasing, true);
 
             anim.SetBool(AnimatorFields.Searching, false);
@@ -116,6 +80,8 @@ namespace State_Machine
 
         private void Patrol(Animator anim)
         {
+            _controller.ClearAgentPath();
+
             anim.SetBool(AnimatorFields.Patrolling, true);
 
             anim.SetBool(AnimatorFields.Searching, false);
